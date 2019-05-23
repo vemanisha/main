e9c5bd4d7d9f8b2f86839acb86ac4c8b07219c37
vemanisha <manishavermaa786@gmail.com>
2019-05-23 13:05:33 -0400

    123456 y

---
+++ src/partTime.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manis
+ */
+public class partTime {
+
+    private int classes;
+
+    /**
+     * Get the value of classes
+     *
+     * @return the value of classes
+     */
+    public int getClasses() {
+        return classes;
+    }
+
+    /**
+     * Set the value of classes
+     *
+     * @param classes new value of classes
+     */
+    public void setClasses(int classes) {
+        this.classes = classes;
+    }
+
+    
+}
