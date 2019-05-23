af58b4ab208ecbbef28ef13e1ef1bd87e1096548
vemanisha <manishavermaa786@gmail.com>
2019-05-23 13:23:39 -0400

    123456 y

---
+++ src/ca/sheridancollege/week3/manisha/manisha.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.manisha;
+
+/**
+ *
+ * @author manis
+ */
+public class manisha {
+    
+    private int noofclass;
+
+    /**
+     * Get the value of noofclass
+     *
+     * @return the value of noofclass
+     */
+    public int getNoofclass() {
+        return noofclass;
+    }
+
+    /**
+     * Set the value of noofclass
+     *
+     * @param noofclass new value of noofclass
+     */
+    public void setNoofclass(int noofclass) {
+        this.noofclass = noofclass;
+    }
+
+}
