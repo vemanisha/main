d45daaf85e1eacc788ba32bd0d715d08e20404db
vemanisha <manishavermaa786@gmail.com>
2019-05-23 12:45:10 -0400

    123456 y

---
+++ src/ca/sheridancollege/week3/manisha/hello.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.manisha;
+
+/**
+ *
+ * @author manis
+ */
+public class hello {
+    private int hello;
+
+    /**
+     * @return the hello
+     */
+    public int getHello() {
+        return hello;
+    }
+
+    /**
+     * @param hello the hello to set
+     */
+    public void setHello(int hello) {
+        this.hello = hello;
+    }
+    
+}
